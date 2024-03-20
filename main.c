092b793bd0b0cea06b6cd5788c228cd5ac89c567
Carlos Aldrim <94694859+carlos-aldrim@users.noreply.github.com>
2024-03-20 18:45:22 -0300

    Update main.c

--- main.c
+++ main.c
@@ -11,18 +11,6 @@ float frequencias_portugues[TAM_ALFABETO] = {
     0.01, 0.47
 };
 
-float frequencias_ingles[TAM_ALFABETO] = {
-    8.17, 1.49, 2.78, 4.25, 12.70, 2.23, 2.02, 6.09, 6.97, 0.15, 0.77, 4.03,
-    2.41, 6.75, 7.51, 1.93, 0.10, 5.99, 6.33, 9.06, 2.76, 0.98, 2.36, 0.15,
-    1.97, 0.07
-};
-
-float frequencias_espanhol[TAM_ALFABETO] = {
-    12.02, 0.92, 1.18, 4.29, 13.11, 0.77, 1.42, 1.08, 4.96, 0.13, 0.17, 5.26,
-    2.22, 4.41, 4.69, 0.31, 0.90, 6.42, 7.02, 3.87, 4.62, 1.05, 0.06, 0.48,
-    0.01, 0.47
-};
-
 void calcular_frequencias(const char *texto, float *frequencias, int tamanho) {
     int total = 0;
     memset(frequencias, 0, tamanho * sizeof(float));
@@ -52,16 +40,7 @@ void decifrar_mensagem(const char *mensagem, const char *idioma) {
     float frequencias[TAM_ALFABETO];
     float *frequencias_idioma;
 
-    if (strcmp(idioma, "portugues") == 0)
-        frequencias_idioma = frequencias_portugues;
-    else if (strcmp(idioma, "ingles") == 0)
-        frequencias_idioma = frequencias_ingles;
-    else if (strcmp(idioma, "espanhol") == 0)
-        frequencias_idioma = frequencias_espanhol;
-    else {
-        printf("Idioma nao suportado.\n");
-        return;
-    }
+    frequencias_idioma = frequencias_portugues;
 
     calcular_frequencias(mensagem, frequencias, TAM_ALFABETO);
 
@@ -101,12 +80,11 @@ int main() {
     char idioma[20];
     printf("Digite a mensagem cifrada: ");
     fgets(mensagem, sizeof(mensagem), stdin);
-    mensagem[strcspn(mensagem, "\n")] = '\0'; // Remover a quebra de linha
+    mensagem[strcspn(mensagem, "\n")] = '\0';
 
-    printf("Digite o idioma (portugues, ingles ou espanhol): ");
-    scanf("%s", idioma);
-
-    decifrar_mensagem(mensagem, idioma);
+    for (int i = 0; mensagem[i]; i++) {
+        mensagem[i] = tolower(mensagem[i]);
+    }
 
     return 0;
 }
